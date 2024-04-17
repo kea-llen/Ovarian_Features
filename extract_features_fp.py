69779e9e3cdb234c7fcc123438f667994ca4d2f2
root <scjjb@leeds.ac.uk>
2024-04-17 13:20:39 +0000

    change model location in extraction

--- extract_features_fp.py
+++ extract_features_fp.py
@@ -303,7 +303,7 @@ if __name__ == '__main__':
             model=timm.create_model('levit_256',pretrained=True, num_classes=0)    
         elif args.model_type=='uni':
             model = timm.create_model("vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True)
-            local_dir = "/mnt/results/vit_large_patch16_224.dinov2.uni_mass100k/"
+            local_dir = "/mount_data/vit_large_patch16_224.dinov2.uni_mass100k/"
             model.load_state_dict(torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location="cpu"), strict=True)
             assert args.use_transforms in ["uni_default"]
         elif args.model_type =='vit_l':
